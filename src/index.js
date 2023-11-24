6d38cfd23987a6c8055f7af4e418beeef43f0e4c
hsanch13 <hayleysanchez13@gmail.com>
2023-11-23 19:15:47 -0600

    added ability to add and delete tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    handleTask(event.target['new-task-description'].value)
+    form.reset()
+  })
 });
+
+function handleTask(newTaskDescription){
+let p = document.createElement('p')
+let btn = document.createElement('button')
+btn.addEventListener('click', handleDelete)
+btn.textContent = 'x'
+p.textContent = `${newTaskDescription} `
+p.appendChild(btn)
+document.querySelector("#tasks").appendChild(p)
+}
+
+function handleDelete(event){
+  event.target.parentNode.remove()
+}
